f16559f35ce409cb7da4c34a560fce7e694e80e0
Joe Rothrock <rothrockindustries@gmail.com>
2019-10-14 15:37:51 -0400

    Initial Commit

---
+++ upload/admin/language/en-gb/extension/module/featured_category.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']    = 'Featured Categories';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified featured categories module!';
+$_['text_edit']        = 'Edit Featured Category Module';
+
+// Entry
+$_['entry_name']       = 'Module Name';
+$_['entry_category']   = 'Categories';
+$_['entry_limit']      = 'Limit';
+$_['entry_column_count'] = 'Columns';
+$_['entry_width']      = 'Image Width';
+$_['entry_height']     = 'Image Height';
+$_['entry_status']     = 'Status';
+
+// Help
+$_['help_category']    = '(Autocomplete)';
+$_['help_column_count']       = 'Number of columns';
+$_['help_limit']       = 'Maximum number of items';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify featured categories module!';
+$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
+$_['error_width']      = 'Width required!';
+$_['error_height']     = 'Height required!';
